e340433082151552ed5d445d5c2e7de87ce6064c
ayoeze191 <ayoeze191@gmail.com>
2024-10-23 08:26:41 -0400

    Axios Naming

--- app/utils/helper/response.tsx
+++ app/utils/helper/response.tsx
@@ -1,31 +1,11 @@
-import { AxiosError } from "axios";
-
-interface DataType {
-  message: string[];
-}
-
-interface AxiosResponse {
-  data: DataType;                
-  status: number;          
-  statusText: string;         
-}
-
-interface ErrorResponseData {
-  message: string;
-  data?: Record<string, unknown>; 
-}
-
-export const handleResponse = (res: AxiosResponse | null, error: AxiosError<ErrorResponseData> | null) => {
-  if (res) {
-    return [res.data, null]; // Return response data if available
-  }
-  
-  return [
-    null,
-    {
-      message: error?.response?.data?.message || "", 
-      data: error?.response?.data?.data || {},       
-      error,                                        
-    },
-  ];
-};
+export const response = (res, error) => {
+    if (res !== null) return [res.data, null];
+    return [
+      null,
+      {
+        message: error?.response?.data?.message || "",
+        data: error?.response?.data?.data || {},
+        error,
+      },
+    ];
+  };
